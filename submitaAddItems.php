52a1a4560b69965d263b7b508c06a34af1a3c290
yuwantha98 <yuwantharavihara123@gmail.com>
2024-10-28 22:39:27 +0530

    remove the debug point

--- submitaAddItems.php
+++ submitaAddItems.php
@@ -8,21 +8,9 @@ if (isset($_POST['submit'])) {
     $itemCode = $_POST['itemCode'];
     $description = $_POST['description'];
 
-    // Check if data is received correctly
-    echo "Received data: <br>";
-    echo "Item Name: $itemName <br>";
-    echo "Price: $price <br>";
-    echo "Quantity: $quantity <br>";
-    echo "Item Code: $itemCode <br>";
-    echo "Description: $description <br>";
-
-    // SQL query to insert data into the item table
     $sql = "INSERT INTO item (Item_name, Item_description, Price, Quantity, Item_code)
             VALUES ('$itemName', '$description', '$price', '$quantity', '$itemCode')";
 
-    echo "SQL Query: $sql <br>"; // Print the SQL query for debugging
-
-    // Execute the query and check for success
     if ($conn->query($sql) === TRUE) {
         echo "Item added successfully!";
     } else {
